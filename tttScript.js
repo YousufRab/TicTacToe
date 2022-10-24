c24219a28b5d51063386f66597d64a57b8377bb7
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-10-24 12:42:38 -0700

    Expand functionality of gameBoard module

--- tttScript.js
+++ tttScript.js
@@ -2,7 +2,12 @@ const gameBoard = (function () {
     board = ["X", "O", "X", "O", "X", "O", "X", "O", "X", "O"];
     const sqrClicked = () => {
         let boardSquares = Array.from(document.querySelectorAll('.boardSqr'));
+        boardSquares.forEach(drawSign)
+    };
 
+    const drawSign = (boardSquare) => {
+        let arraySign = 0;
+        boardSquare
     }
     return {board};
 })();
